6bfa2f78ec15b51e69b8009e5c5c511b1e78b01d
KartikayGoyal <55656513+KartikayGoyal@users.noreply.github.com>
2020-09-21 03:17:41 +0530

    Add files via upload
    These are my files of the whole project.

---
+++ IMAGE_GALLERY/ImageItem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.IO;
+
+namespace IMAGE_GALLERY
+{
+    public class ImageItem
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Base64 { get; set; }
+        public string Format { get; set; }
+    }
+    public class DataFetcher
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Base64 { get; set; }
+        public string Format { get; set; }
+        async Task<string> GetDatafromService(string searchstring)
+        {
+            string readText = null;
+            try
+            {
+                var azure = @"https://imagefetcher20200529182038.azurewebsites.net";
+
+               string url = azure + @"/api/fetch_images?query=" + searchstring + "&max_count=5";
+
+               using (HttpClient c = new HttpClient())
+                {
+                    readText = await c.GetStringAsync(url);
+                }
+            }
+            catch
+            {
+                readText = File.ReadAllText(@"Data/sampleData.json");
+
+            }
+            return readText;
+        }
+        public async Task<List<ImageItem>> GetImageData(string search)
+        {
+            string data = await GetDatafromService(search);
+            return JsonConvert.DeserializeObject<List<ImageItem>>(data);
+        }
+
+    }
+    
+ 
+}
